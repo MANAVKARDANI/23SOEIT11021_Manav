9f0aa5ee8b3a4a4bd14e47e396295512ea81186a
Kardani Manav <manavkardani75@gmail.com>
2025-07-18 10:24:12 +0530

    18/07/2025

--- Class1.cs
+++ Class1.cs
@@ -8,6 +8,15 @@ namespace project
 {
     internal class Class1
     {
+        static void Main(string[] args)
+        {
+            int[] a = { 2, 1, 4, 6, 8, 9, 10 };
+            a.Max();
+            Console.WriteLine("Max alement" + a.Max());
+            Console.WriteLine("Max alement" + a.Min());
+            Console.WriteLine("Max alement" + a.Reverse());
 
+
+        }
     }
 }
